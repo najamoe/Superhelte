29ca3a88d4898072fa981ebb07fc6db7f89c8352
najamoe <naja0718@stud.kea.dk>
2022-09-12 19:57:44 +0200

    2nd commit - attr, construc, gettere,

--- src/main/java/Superhero.java
+++ src/main/java/Superhero.java
@@ -1,5 +1,46 @@
 public class Superhero {
 
+    //Attributes
+    private String navn;
+    private String superheltenavn;
+    private String superkraft;
+    private boolean menneske;
+    private double oprindelsesår;
+    private double Styrke;
 
+    //Constructor
+    public Superhero(String navn, String superheltenavn, String superkraft, boolean menneske, double oprindelsesår, double styrke) {
+        this.navn = navn;
+        this.superheltenavn = superheltenavn;
+        this.superkraft=superkraft;
+        this.menneske=menneske;
+        this.oprindelsesår=oprindelsesår;
+        this.Styrke=styrke;
+    }
 
+    //Gettere
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public String getSuperheltenavn() {
+        return superheltenavn;
+    }
+
+    public String getSuperkraft() {
+        return superkraft;
+    }
+
+    public boolean isMenneske() {
+        return menneske;
+    }
+
+    public double getOprindelsesår() {
+        return oprindelsesår;
+    }
+
+    public double getStyrke() {
+        return Styrke;
+    }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String [] args){
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Velkommen til superheltene");
+
+
 
         //superhelte-objekter
-
+        //Superhero helt1 = new Superhero("Flash", "Superspeed", true, "1995", "10")
 
     }
 
